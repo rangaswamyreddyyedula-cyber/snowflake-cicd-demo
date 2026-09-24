c0fd77a36f9ba4895b86bf13fa32e784052ce3ca
rangaswamyreddyyedula-cyber <rangaswamyreddyyedula@gmail.com>
2026-09-24 22:04:51 +0530

    Create versions.tf
    Which Terraform and Snowflake provider versions to use.

---
+++ terraform/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = ">= 1.0.0, < 3.0.0"
+    }
+  }
+}
